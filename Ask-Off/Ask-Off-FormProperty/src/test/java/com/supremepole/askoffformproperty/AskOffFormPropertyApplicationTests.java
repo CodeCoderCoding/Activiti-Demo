5930b27ecd83474368724636a000e516328a30a7
CodeCoderCoding <1845371176@qq.com>
2022-04-18 23:21:09 +0800

    Add static files in Ask-Modeler module

---
+++ Ask-Off/Ask-Off-FormProperty/src/test/java/com/supremepole/askoffformproperty/AskOffFormPropertyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.supremepole.askoffformproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskOffFormPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
